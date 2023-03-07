7540a938a06f5fad9c9a8c778dd695b3ca34edd3
Hyggge <1695386157@qq.com>
2023-03-07 19:02:07 +0800

    fix(fileparser): 将整个字符串传入words数组中，并去重

--- src/lib/FileParser.cpp
+++ src/lib/FileParser.cpp
@@ -1,6 +1,8 @@
 #include "FileParser.h"
 #include <string>
 #include <fstream>
+#include <vector>
+#include <set>
 
 FileParser::FileParser(const std::string &filename) {
     std::ifstream ifs;
@@ -8,26 +10,30 @@ FileParser::FileParser(const std::string &filename) {
     if (ifs.fail()) {
         // todo: throw exception
     }
-
-    char head = '\0', last = '\0';
-    bool isParsingWord = false;
+    std::vector<char> buffer;
+    std::set<std::string> wordSet;
     this->wordsNum = 0;
 
 
     while (! ifs.eof()) {
         char ch = (char)ifs.get();
-        if (isalpha(ch) && !isParsingWord) {
-            head = ch;
-            isParsingWord = true;
-        } else if (!isalpha(ch) && isParsingWord) {
-            this->words[this->wordsNum] = (char *) malloc(3 * sizeof(char));
-            this->words[this->wordsNum][0] = head;
-            this->words[this->wordsNum][1] = last;
-            this->words[this->wordsNum][2] = '\0';
-            this->wordsNum++;
-            isParsingWord = false;
+        if (isalpha(ch)) {
+            buffer.push_back(ch);
         }
-        last = ch;
+        else if (! isalpha(ch) && ! buffer.empty()) {
+            int size = (int)buffer.size();
+            std::string word = std::string(buffer.data(), buffer.size());
+            if (wordSet.count(word) == 0) {
+                this->words[this->wordsNum] = (char *) malloc((size + 1) * sizeof(char));
+                for (int i = 0; i < size; ++i) {
+                    this->words[this->wordsNum][i] = buffer[i];
+                }
+                this->words[this->wordsNum][size] = '\0';
+                this->wordsNum++;
+                wordSet.insert(word);
+            }
+            buffer.clear();
+        }
     }
 }
 
