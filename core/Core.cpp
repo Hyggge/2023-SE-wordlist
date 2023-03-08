eea56a1bf7e33e1d021d9e2246bfc6a7bed70d09
温佳昊 <20373668@buaa.edu.cn>
2023-03-08 17:30:35 +0800

    feat(core graph): 完成了Core.cpp和Graph.h中的接口

--- core/Core.cpp
+++ core/Core.cpp
@@ -1,13 +1,35 @@
 #include "Core.h"
-int gen_chains_all(char* words[], int len, char* result[]) {
+#include "Graph.h"
+#include <stdexcept>
 
-    return 0;
+int gen_chains_all(char* words[], int len, char* result[]) {
+    Graph graph(words, len);
+    if (graph.hasCircle()) {
+        throw std::logic_error("Circle detected");
+    }
+    return graph.genChainsAll(result);
 }
 
 int gen_chain_word(char* words[], int len, char* result[], char head, char tail, char except, bool enable_loop) {
-    return 0;
+    Graph graph(words, len, except);
+    if (graph.hasCircle()) {
+        if (!enable_loop) {
+            throw std::logic_error("Circle detected, please add -r to support circle");
+        }
+        return graph.genChainWordWithCircle(result, head, tail);
+    } else {
+        return graph.genChainWordWithoutCircle(result, head, tail);
+    }
 }
 
 int gen_chain_char(char* words[], int len, char* result[], char head, char tail, char except, bool enable_loop) {
-    return 0;
+    Graph graph(words, len, except);
+    if (graph.hasCircle()) {
+        if (!enable_loop) {
+            throw std::logic_error("Circle detected, please add -r to support circle");
+        }
+        return graph.genChainCharWithCircle(len, result, head, tail);
+    } else {
+        return graph.genChainCharWithoutCircle(result, head, tail);
+    }
 }
--- core/include/Graph.h
+++ core/include/Graph.h
@@ -1,13 +1,32 @@
 #ifndef WORDLIST_GRAPH_H
 #define WORDLIST_GRAPH_H
 
-struct Edge {
+#include <vector>
 
+struct Edge {
+    int wordId, to;
 };
 
 class Graph {
+private:
+    std::vector<Edge> g[26];
 
+    void addEdge(int u, int v, int wordId);
 
+public:
+    Graph(char* words[], int len, char except = '\0');
+
+    bool hasCircle();
+
+    int genChainsAll(char* result[]);
+
+    int genChainWordWithCircle(char* result[], char head, char tail);
+
+    int genChainWordWithoutCircle(char* result[], char head, char tail);
+
+    int genChainCharWithCircle(int len, char* result[], char head, char tail);
+
+    int genChainCharWithoutCircle(char* result[], char head, char tail);
 };
 
 #endif //WORDLIST_GRAPH_H
