3123c62224737b63b9d80fa58f654311cbbec79e
Hyggge <1695386157@qq.com>
2023-03-14 17:08:48 +0800

    feat(main): 当cli用户没有指定任何参数时，打印Usage

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,29 @@
 #include "Controller.h"
 #include <iostream>
 
+void printUsage() {
+    char usage[] = "\n"
+                   "Usage: \n"
+                   "  Wordlist <function> <filename> [options]\n"
+                   "\n"
+                   "Functional Options:\n"
+                   "  -n             Get the total number of word chains.\n"
+                   "  -w             Get the word chain with the most words.\n"
+                   "  -c             Get the word chain with the most letters.\n"
+                   "\n"
+                   "Additional Options:\n"
+                   "  -h <head>      Specify the first letter of the chain.\n"
+                   "  -t <tail>      Specify the last letter of the chain.\n"
+                   "  -j <except>    Specifies a letter that cannot be the first letter of any word in the chain.\n"
+                   "  -r             Allow implicit word circles.\n"
+                   "\n";
+    printf("%s", usage);
+    exit(0);
+}
+
 int main(int argc, char* argv[]) {
+    if (argc == 1) printUsage();
+
     try {
         ArgParser argParser = ArgParser(argc, argv);
         UserOptions userOptions = argParser.getOptions();
